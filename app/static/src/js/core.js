7ba9c112d444648f60d510c9b82685e19f85f70b
Truong Phan <dangtruong@gmail.com>
2021-03-07 16:02:37 +0700

    Fixed assest bundle issue

---
+++ app/static/src/js/core.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+	//Javascript to toggle the menu
+	document.getElementById("nav-toggle").onclick = function () {
+		document.getElementById("nav-content").classList.toggle("hidden");
+	};
+
+	$(".modal-overlay").click(toggleModal);
+});
+
+let toggleModal = () => {
+	const modal = document.querySelector(".modal");
+	modal.classList.toggle("opacity-0");
+	modal.classList.toggle("pointer-events-none");
+};
+
+let preview = (word) => {
+	$("#modal-content").empty();
+	$("#modal-header").hide();
+	$("#loading").show();
+	toggleModal();
+	$.get(`/api/lookup/${word}`, (data) => {
+		$("#loading").hide();
+		if (data.length > 0) {
+			$("#modal-header").show();
+			let r_count = 1;
+			$("#modal-content").append(`<h1 class="text-yellow-800 text-3xl font-semibold uppercase">${word}</h1>`);
+			for (let r of data) {
+				$("#modal-content").append(`<h2 class="text-lg mt-2 font-semibold text-yellow-600">${r_count}.</h2>`);
+				for (let e of r.sense) {
+					$("#modal-content").append(`<h3 class="text-base text-gray-400 italic font-mono">(${e.lexical})</h3>`);
+					for (let d of e.entry) {
+						$("#modal-content").append(`<p></p>`);
+						$("#modal-content").append(`<p class="font-semibold">${d.definition}</p>`);
+						$("#prevmodal-contentiew").append(`<p></p>`);
+						for (let x of d.examples) $("#modal-content").append(`<p class="italic">- ${x.text} </p>`);
+					}
+					$("#modal-content").append(`<p class="my-2"></p>`);
+				}
+				r_count++;
+			}
+		} else {
+			$("#modal-content").append(`<h2 class="text-lg mt-2 font-semibold text-red-900">No entry found matching supplied </h2>`);
+			$("#modal-header").hide();
+		}
+	});
+};
