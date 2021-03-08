3738407ee7ad97c201125ef4d4698f36095a246b
Truong Phan <dangtruong@gmail.com>
2021-03-08 20:27:16 +0700

    minor UI fix

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ module.exports = (ctx) => ({
 		ctx.env === "production" &&
 			require("@fullhuman/postcss-purgecss")({
 				content: [path.resolve(__dirname, "app/templates/**/*.html")],
+				css: [path.resolve(__dirname, "app/static/src/*.css")],
 				defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 			}),
 	],
